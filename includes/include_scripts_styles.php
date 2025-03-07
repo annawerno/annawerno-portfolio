9815405a8345961125be3b1eaeac91d5c5b7d2c8
Anna <annawerno@hotmail.co.uk>
2025-03-07 07:17:02 +0000

    initial commit

---
+++ includes/include_scripts_styles.php
@@ -0,0 +1,9 @@
+<?php
+function scripts_styles() {
+
+    wp_enqueue_style('styles', get_template_directory_uri() . '/src/css/style.css', array(), '1.0');
+
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/src/js/script.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'scripts_styles');
+?>
