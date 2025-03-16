9fde7020da70b410774f5b22af5e5e63e8a21a6a
Anna <annawerno@hotmail.co.uk>
2025-03-16 19:41:06 +0000

    footer

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -5,3 +5,16 @@ function toggleSidebar() {
   //   ? "250px"
   //   : "60px";
 }
+
+document.getElementById("nav-toggle").addEventListener("click", function () {
+  this.classList.toggle("open");
+  document.querySelector(".header__menu").classList.toggle("active");
+});
+
+// Close menu when a menu item is clicked
+document.querySelectorAll(".header__menu a").forEach((item) => {
+  item.addEventListener("click", function () {
+    document.getElementById("nav-toggle").classList.remove("open");
+    document.querySelector(".header__menu").classList.remove("active");
+  });
+});
--- templates/t__header.php
+++ templates/t__header.php
@@ -5,7 +5,14 @@
           <img src="<?= site_url(); ?>/wp-content/uploads/2025/03/annawerno-logo.webp" alt="annawerno logo" width="100%"/>
         </div>
         <div class="" style="display: flex">
-          <button class="btn-contact">Let's talk</button>
+          <button class="btn-contact">Let's chat</button>
+
+          <button class="header__menu-mob" aria-label="Toggle menu" id="nav-toggle">
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+
 
           <?php wp_nav_menu(array(
             'theme_location' => 'primary-menu',
