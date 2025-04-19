152efdc9c0d7125bc92ebe41018ac5a9a533ce71
Anna <annawerno@hotmail.co.uk>
2025-04-19 07:02:52 +0100

    fixes after first migration

--- templates/t__footer.php
+++ templates/t__footer.php
@@ -1,6 +1,7 @@
 <footer id="footer__container">
   <section class="container__fluid footer__main">
     
+
       <?php wp_nav_menu(array(
           'theme_location' => 'footer-menu',
           'container' => 'nav',
@@ -11,10 +12,10 @@
         <img src="<?= site_url(); ?>/wp-content/uploads/2025/03/annawerno-logo-light.webp" alt="annawerno logo" width="100%"/>
       </a>
 
-  </section>
-  <section class="container__fluid footer__sub">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> All Rights Reserved
-  </section>
-  <button class="scroll-btt" onclick="window.scrollTo({top: 0, behavior: 'smooth'});"><?= file_get_contents(get_template_directory() . '/img/icons/back-to-top.svg'); ?></button>
+      <button class="scroll-btn" onclick="window.scrollTo({top: 0, behavior: 'smooth'});"><?= file_get_contents(get_template_directory() . '/img/icons/back-to-top.svg'); ?></button>
 
+      <p class="footer__copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> All Rights Reserved</p>
+
+  </section>
 
 </footer>
